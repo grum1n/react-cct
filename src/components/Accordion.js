438ce4bb84ff5365f5e38827f43c14464cbb9396
grum1n <grumin.el.pastas@gmail.com>
2021-11-14 20:49:52 +0200

    added Wrap style

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -44,6 +44,46 @@ const WrapDiv = styled.div`
     cursor:pointer;
     box-shadow: 0 0 20px #c5c5c5;
 `
+const Wrap = styled.div`
+    background:#fff;
+    padding:16px 30px;
+    color:rgba(14,42,53,1);
+    position:relative;
+    display: flex;
+    justify-content: flex-start;
+    align-items:center;
+    width:100%;
+    text-align:center;
+    cursor:pointer;
+    
+    position:absolute;
+    height: 98px;
+    top:0;
+    left:0;
+
+    h1 {
+        margin:0;
+        font-family: 'Manrope', sans-serif;
+        font-size:18px;
+        font-weight:700;
+        line-height:24px;
+       
+    }
+    span {
+        margin: 0 36px 0 0;
+        width: 50px;
+        height:53px;
+        display:flex;
+        justify-content:center;
+        align-items:center;
+        font-size:2rem;
+        color:rgba(255,255,255,1);
+        background: linear-gradient(180deg, #00a9a7 0%, #00ba89 10% );
+    }
+  
+`;
+
+
 
 const Accordion = () => {
     return (
@@ -54,8 +94,10 @@ const Accordion = () => {
                     return (
                         <WrapContainer>
                             <WrapDiv>
-                                <span>{item.number}</span>
-                                <h1>{item.title}</h1>
+                                <Wrap>
+                                    <span>{item.number}</span>
+                                    <h1>{item.title}</h1>
+                                </Wrap>
                             </WrapDiv>
                         </WrapContainer>
                         )
