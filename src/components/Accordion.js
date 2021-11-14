b169a7a7a5502016638553c4ec8858cb281a925d
grum1n <grumin.el.pastas@gmail.com>
2021-11-14 20:35:01 +0200

    added AccordionSection

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -3,11 +3,21 @@ import {Data} from './Data';
 import styled from 'styled-components';
 import Image from './images/Path2337.png';
 
+const AccordionSection = styled.div`
+    display: flex;
+    flex-direction:column;
+    align-items:center;
+    justify-content:center;
+    position:relative;
+    height:100vh;
+    background:#fff;
+`;
+
 const Accordion = () => {
     return (
-        <div>
+        <AccordionSection>
              cct acccordion
-        </div>
+        </AccordionSection>
     )
 }
 
