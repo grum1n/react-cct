8faf9f2a2e05a9ed786cf1546e96529b1b62ae63
grum1n <grumin.el.pastas@gmail.com>
2021-11-14 20:32:16 +0200

    imported some files to Accordion.js

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Data} from './Data';
+import styled from 'styled-components';
+import Image from './images/Path2337.png';
 
 const Accordion = () => {
     return (
