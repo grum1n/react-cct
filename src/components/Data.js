f593ae95fdb190ee6a686764b9b632c5ffec6482
grum1n <grumin.el.pastas@gmail.com>
2021-11-14 20:24:46 +0200

    added new file Data.js and code

---
+++ src/components/Data.js
@@ -0,0 +1,62 @@
+export const Data = [
+    {
+        number: '1',
+        title: 'Build test task',
+        content: [
+            {
+                text: 'Create repository',
+                email: 'blabla@cornercasetech.com'
+            },
+            {
+                text:'Implementdesigns',
+                email: 'xxx@ggg.com'
+            },
+            {
+                text: 'Implement functionality',
+                email: ''
+            }
+        ]
+    },
+    {
+        number: '2',
+        title: 'Submit your test task',
+        content: [
+           { 
+                text: 'Open email client',
+                email: ''
+            },
+            { 
+                text: 'Open email client',
+                email: "carerrs@cornercasetech.com"
+            }
+        ]
+    },
+    {
+        number: '3',
+        title: 'Participate in tech interview',
+        content: [
+            { 
+                text: 'Open email client',
+                email: ''
+            },
+            { 
+                text: 'Open email client',
+                email: "carerrs@cornercasetech.com"
+            }
+        ]
+    },
+    {
+        number: '4',
+        title: 'Receive answer',
+        content: [
+            { 
+                text: 'Open email client',
+                email: ''
+            },
+            { 
+                text: 'Open email client',
+                email: ""
+            }
+        ]
+    }
+];
